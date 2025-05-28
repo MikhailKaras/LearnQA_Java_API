0a660fbdb0711e2044d2a9d9ce121b1eef01f19b
MikhailKaras <misha.karas.80@mail.ru>
2025-05-28 17:23:46 +0300

    Hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public void hellofromme() {
+        System.out.println("Hello from Mikhail");
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.hellofromme();
+    }
+}
