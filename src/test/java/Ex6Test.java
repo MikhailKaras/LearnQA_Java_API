708ceaf74cf8b24d699c9594c7866c7fe3daf566
MikhailKaras <misha.karas.80@mail.ru>
2025-06-07 20:15:58 +0300

    Ex6: Редирект

--- src/test/java/Ex6Test.java
+++ src/test/java/Ex6Test.java
@@ -1,15 +1,10 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
+
 
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import io.restassured.http.Headers;
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Ex6Test {
     @Test
@@ -18,11 +13,11 @@ public class Ex6Test {
         Response response = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
                 .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
-        String location = response.getHeader("x-host");
+        String location = response.getHeader("Location");
         System.out.println(location);
     }
 }
